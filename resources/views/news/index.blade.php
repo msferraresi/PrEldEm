40c64350c79494c9d5b1fe4567ada7abf3c6a3fb
Ferraresi Mauro <mauro.ferraresi@ulusoft.com.ar>
2022-11-09 01:14:38 -0300

    Se corrigen los template de blade

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -4,56 +4,52 @@
             Novedades
         </h2>
     </x-slot>
+    <div class="max-w-10xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200 w-full">
+                            <thead>
+                            <tr>
+                                <th hidden>
+                                    ID
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Titulo
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Descripcion
+                                </th>
+                                <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
 
-    <div>
-        <div class="max-w-10xl mx-auto py-10 sm:px-6 lg:px-8">
-            <div class="flex flex-col">
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table class="min-w-full divide-y divide-gray-200 w-full">
-                                <thead>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($news as $new)
                                 <tr>
-                                    <th hidden>
-                                        ID
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Titulo
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Descripcion
-                                    </th>
-                                    <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
+                                    <td hidden>
+                                        {{ $new->id }}
+                                    </td>
 
-                                    </th>
+                                    <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-900">
+                                        {{ $new->tittle }}
+                                    </td>
+                                    <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-900">
+                                        {{ $new->description }}
+                                    </td>
+
+                                    <td class="px-6 py-3 whitespace-nowrap text-sm font-small">
+                                        <a href="{{ route('news.show', $new->id) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Ver</a>
+                                    </td>
                                 </tr>
-                                </thead>
-                                <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($news as $new)
-                                    <tr>
-                                        <td hidden>
-                                            {{ $new->id }}
-                                        </td>
-
-                                        <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $new->tittle }}
-                                        </td>
-                                        <td class="px-6 py-3 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $new->description }}
-                                        </td>
-
-                                        <td class="px-6 py-3 whitespace-nowrap text-sm font-small">
-                                            <a href="{{ route('news.show', $new->id) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Ver</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </x-app-layout>
--- resources/views/rrhh/news/create.blade.php
+++ resources/views/rrhh/news/create.blade.php
@@ -4,37 +4,45 @@
             Crear Noticia
         </h2>
     </x-slot>
-    @can('novedades.edit')
-        <div>
-            <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
-                <div class="mt-5 md:mt-0 md:col-span-2">
-                    <form method="post" action="{{ route('news.store') }}">
-                        @csrf
-                        <div class="shadow overflow-hidden sm:rounded-md">
-                            <div class="px-4 py-5 bg-white sm:p-6">
-                                <label for="tittle" class="block font-medium text-sm text-gray-700">Titulo</label>
-                                <input type="text" name="tittle" id="tittle" type="text" class="form-input rounded-md shadow-sm mt-1 block w-full"
-                                    value="{{ old('tittle', '') }}" />
-                                @error('tittle')
-                                    <p class="text-sm text-red-600">{{ $message }}</p>
-                                @enderror
-                            </div>
-                            <div class="px-4 py-5 bg-white sm:p-6">
-                                <label for="description" class="block font-medium text-sm text-gray-700">Descripcion</label>
-                                <input type="text" name="description" id="description" type="text" class="form-input rounded-md shadow-sm mt-1 block w-full"
-                                    value="{{ old('description', '') }}" />
-                                @error('description')
-                                    <p class="text-sm text-red-600">{{ $message }}</p>
-                                @enderror
-                            </div>
-
-                            <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-                                    Crear
-                                </button>
-                            </div>
+    @can('novedades.create')
+        <div class="max-w-10xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="block mb-8">
+                            <a href="{{ route('rrhh.index_news') }}" class="inline-flex items-center px-4 py-2  bg-gray-200   border border-transparent rounded-md font-semibold text-xs text-black uppercase  hover:bg-gray-400 active:bg-gray-900 focus:outline-none focus:border-gray-900 disabled:opacity-25 transition">Volver</a>
                         </div>
-                    </form>
+                        <br>
+                        <div class="mt-5 md:mt-0 md:col-span-2">
+                            <form method="post" action="{{ route('news.store') }}">
+                                @csrf
+                                <div class="shadow overflow-hidden sm:rounded-md">
+                                    <div class="px-4 py-5 bg-white sm:p-6">
+                                        <label for="tittle" class="block font-medium text-sm text-gray-700">Titulo</label>
+                                        <input type="text" name="tittle" id="tittle" type="text" class="form-input rounded-md shadow-sm mt-1 block w-full"
+                                            value="{{ old('tittle', '') }}" />
+                                        @error('tittle')
+                                            <p class="text-sm text-red-600">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                    <div class="px-4 py-5 bg-white sm:p-6">
+                                        <label for="description" class="block font-medium text-sm text-gray-700">Descripcion</label>
+                                        <input type="text" name="description" id="description" type="text" class="form-input rounded-md shadow-sm mt-1 block w-full"
+                                            value="{{ old('description', '') }}" />
+                                        @error('description')
+                                            <p class="text-sm text-red-600">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                                            Crear
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
